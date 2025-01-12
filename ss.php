a843dfe9f60634b8bbcc5f7ab8cc21d0a10f9ce9
Michael Steenbeek <m.o.steenbeek@gmail.com>
2025-01-12 15:13:16 +0100

    Fix copy-paste error in ss.php

--- ss.php
+++ ss.php
@@ -1,6 +1,6 @@
 <?php
 $filename = basename($_SERVER['REQUEST_URI']);
-if (str_starts_with($filename, 'index.php?') && file_exists($filename))
+if (str_starts_with($filename, 'ss.php?') && file_exists($filename))
 {
     include $filename;
     exit(0);
